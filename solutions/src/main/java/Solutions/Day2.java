42c599e239cc51d9fb247954b2e0b9eec1223095
Krishna-Teja732 <b.krishnateja732@gmail.com>
2024-05-12 13:29:39 +0530

    Advent of Code 2023 - Day 2

--- solutions/src/main/java/Solutions/Day2.java
+++ solutions/src/main/java/Solutions/Day2.java
@@ -1,6 +1,87 @@
 package Solutions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class Day2 {
-	public Day2() {
+
+	int[] maxRGB;
+
+	public Day2(int[] maxRGB) {
+		this.maxRGB = maxRGB;
 	}
+
+	public boolean isGamePossible(int[] rgb) {
+		for (int index = 0; index < 3; index++) {
+			if (maxRGB[index] < rgb[index]) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public int totalPossibleGames(List<List<int[]>> games) {
+		int result = 0;
+
+		for (int index = 0; index < games.size(); index++) {
+			boolean gameIsPossible = true;
+			for (int[] rgb : games.get(index)) {
+				gameIsPossible = gameIsPossible && isGamePossible(rgb);
+				if (!gameIsPossible) {
+					break;
+				}
+			}
+			if (gameIsPossible) {
+				result = result + index + 1;
+			}
+		}
+
+		return result;
+	}
+
+	public double totalPossibleGamesII(List<List<int[]>> games) {
+		double result = 0;
+		for (int index = 0; index < games.size(); index++) {
+			int[] curRGBvalues = new int[3];
+			for (int[] rgb : games.get(index)) {
+				for (int color = 0; color < 3; color++) {
+					curRGBvalues[color] = Math.max(curRGBvalues[color], rgb[color]);
+				}
+			}
+			result = result + curRGBvalues[0] * curRGBvalues[1] * curRGBvalues[2];
+
+		}
+
+		return result;
+	}
+
+	public List<int[]> formatLine(String line) {
+		List<int[]> result = new ArrayList<>();
+		String[] rounds = line.substring(line.indexOf(":") + 1).split(";");
+		for (String round : rounds) {
+			int[] rgb = new int[3];
+			for (String turn : round.split(",")) {
+				turn = turn.strip();
+				switch (turn.substring(turn.length() - 3, turn.length())) {
+					case "red" ->
+						rgb[0] = Integer.parseInt(turn.substring(0, turn.length() - 3).strip());
+					case "een" ->
+						rgb[1] = Integer.parseInt(turn.substring(0, turn.length() - 5).strip());
+					case "lue" ->
+						rgb[2] = Integer.parseInt(turn.substring(0, turn.length() - 4).strip());
+				}
+			}
+			result.add(rgb);
+		}
+		return result;
+	}
+
+	public List<List<int[]>> formatInput(List<String> inputLines) {
+		List<List<int[]>> result = new ArrayList<>();
+		for (String line : inputLines) {
+			result.add(formatLine(line));
+		}
+		return result;
+	}
+
 }
--- solutions/src/main/java/Solutions/Main.java
+++ solutions/src/main/java/Solutions/Main.java
@@ -7,7 +7,8 @@ import java.util.List;
 
 class Main {
 	public static void main(String[] args) throws IOException {
-		List<String> inputs = new ReadFileHelper("inputs/day1.txt").getAllLines();
-		System.out.println(new Day1().sumOfCalibrationValuesII(inputs));
+		List<String> inputs = new ReadFileHelper("inputs/day2.txt").getAllLines();
+		Day2 obj = new Day2(new int[] { 12, 13, 14 });
+		System.out.println(obj.totalPossibleGamesII(obj.formatInput(inputs)));
 	}
 }
