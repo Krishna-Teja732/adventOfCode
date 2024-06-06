ca83fb28d18f9304989d43b1f19e5bdecb362ea0
Krishna-Teja732 <b.krishnateja732@gmail.com>
2024-06-06 20:34:10 +0530

    Day 8

---
+++ solutions/src/main/java/Solutions/Day8.java
@@ -0,0 +1,107 @@
+package Solutions;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+class Day8 {
+	private List<Node> startingNodes;
+	private char[] path;
+
+	public Day8(List<String> inputs) {
+		formatInput(inputs);
+	}
+
+	public double findStepsToDestination() {
+		double[] counts = new double[startingNodes.size()];
+		int index = 0;
+
+		for (Node node : startingNodes) {
+			counts[index++] = findStepsToDestination(node);
+		}
+
+		double lcm = counts[0];
+
+		for (index = 1; index < startingNodes.size(); index++) {
+			lcm = counts[index] * lcm / computeGCD(counts[index], lcm);
+		}
+		return lcm;
+	}
+
+	public double computeGCD(double a, double b) {
+		if (b == 0) {
+			return a;
+		}
+		return computeGCD(b, a % b);
+	}
+
+	private int findStepsToDestination(Node root) {
+		int pathIndex = 0;
+		int steps = 0;
+
+		while (!root.isDestination) {
+			switch (path[pathIndex]) {
+				case 'R' -> root = root.right;
+				default -> root = root.left;
+			}
+			pathIndex = (pathIndex + 1) % path.length;
+			steps = steps + 1;
+		}
+
+		return steps;
+	}
+
+	private Node getNodeFromName(String name, HashMap<String, Node> nameToNodeMap) {
+		if (!nameToNodeMap.containsKey(name)) {
+			nameToNodeMap.put(name, new Node(name));
+		}
+		return nameToNodeMap.get(name);
+	}
+
+	private void formatInput(List<String> inputs) {
+		startingNodes = new ArrayList<>();
+		HashMap<String, Node> nameToNodeMap = new HashMap<>();
+
+		path = inputs.getFirst().toCharArray();
+
+		for (int index = 2; index < inputs.size(); index++) {
+			String input = inputs.get(index);
+
+			Node curNode = getNodeFromName(input.substring(0, 3), nameToNodeMap);
+
+			if (curNode.value.endsWith("A")) {
+				this.startingNodes.add(curNode);
+			} else if (curNode.value.endsWith("Z")) {
+				curNode.isDestination = true;
+			}
+
+			curNode.left = getNodeFromName(input.substring(7, 10), nameToNodeMap);
+			curNode.right = getNodeFromName(input.substring(12, 15), nameToNodeMap);
+		}
+	}
+
+	class Node {
+		String value;
+
+		Node right;
+		Node left;
+
+		public boolean isDestination;
+
+		public Node(String value) {
+			this.value = value;
+			isDestination = false;
+		}
+
+		public Node(String value, Node right, Node left) {
+			this.value = value;
+			this.right = right;
+			this.left = left;
+			isDestination = false;
+		}
+
+		public String toString() {
+			return value;
+		}
+	}
+}
--- solutions/src/main/java/Solutions/Main.java
+++ solutions/src/main/java/Solutions/Main.java
@@ -3,11 +3,12 @@ package Solutions;
 import Solutions.utils.ReadFileHelper;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.util.List;
 
 class Main {
 	public static void main(String[] args) throws IOException {
-		List<String> inputs = new ReadFileHelper("inputs/day7.txt").getAllLines();
-		System.out.println(new Day7(inputs).getTotalValue());
+		List<String> inputs = new ReadFileHelper("inputs/day8example.txt").getAllLines();
+		System.out.println(new BigDecimal(new Day8(inputs).findStepsToDestination()).toPlainString());
 	}
 }
