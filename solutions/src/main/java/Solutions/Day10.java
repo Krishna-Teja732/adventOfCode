dd0f4743fae6086ace35bc15e0968b469e77da85
Krishna-Teja732 <b.krishnateja732@gmail.com>
2024-06-21 01:31:30 +0530

    Day 10 part 1

--- solutions/src/main/java/Solutions/Day10.java
+++ solutions/src/main/java/Solutions/Day10.java
@@ -1,55 +1,134 @@
 package Solutions;
 
 import java.util.List;
-import java.util.ArrayList;
+
+import Solutions.utils.Coordinate;
+import Solutions.utils.Pair;
 
 class Day10 {
 
-	private List<List<Character>> grid;
-	private int startX;
-	private int startY;
+	private char[][] grid;
+	private Coordinate startCoordinate;
 	private int maxSteps;
 
 	public Day10(List<String> inputs) {
 		maxSteps = 0;
+		grid = new char[inputs.size()][];
 		formatInput(inputs);
 	}
 
 	private void formatInput(List<String> inputs) {
-		grid = new ArrayList<>();
-
 		for (int index = 0; index < inputs.size(); index++) {
-			List<Character> gridRow = new ArrayList<>();
 			char[] line = inputs.get(index).toCharArray();
+			grid[index] = new char[line.length];
 			for (int charInd = 0; charInd < line.length; charInd++) {
 				if (line[charInd] == 'S') {
-					startX = index;
-					startY = charInd;
+					startCoordinate = new Coordinate(index, charInd);
 				}
-				gridRow.add(line[charInd]);
+				grid[index][charInd] = line[charInd];
 			}
-			grid.add(gridRow);
 		}
 	}
 
-	public int getBiggestLoop() {
-		dfs(startX, startY, 0);
+	public int getResult() {
+		for (Direction direction : Direction.values()) {
+			dfs(new Pair<>(this.startCoordinate.add(direction.unitVector), direction), 1);
+		}
 		return this.maxSteps / 2;
 	}
 
-	private void dfs(int x, int y, int steps) {
-		if (x == startX && y == startY) {
+	private void dfs(Pair<Coordinate, Direction> vector, int steps) {
+		while (vector != null && isValidCoordinate(vector.key) && !vector.key.equals(this.startCoordinate)) {
+			steps++;
+			vector = getValidNeighbor(vector.key, vector.value);
+		}
+		if (vector != null && isValidCoordinate(vector.key) && vector.key.equals(this.startCoordinate)) {
 			maxSteps = Math.max(maxSteps, steps);
 			return;
 		}
-
-		for (int[] positions : getValidNeighbors(x, y)) {
-			dfs(positions[0], positions[1], steps + 1);
-		}
 	}
 
-	private List<int[]> getValidNeighbors(int x, int y) {
-		// #TODO
-		return new ArrayList<>();
+	private Pair<Coordinate, Direction> getValidNeighbor(Coordinate c, Direction direction) {
+		Direction d = null;
+		switch (grid[c.x][c.y]) {
+			case '-' -> {
+				if (direction.equals(Direction.EAST) || direction.equals(Direction.WEST)) {
+					d = direction;
+				}
+			}
+			case '|' -> {
+				if (direction.equals(Direction.NORTH) || direction.equals(Direction.SOUTH)) {
+					d = direction;
+				}
+			}
+			case 'F' -> {
+				if (direction.equals(Direction.WEST) || direction.equals(Direction.NORTH)) {
+					d = reflect(direction.unitVector, -1);
+				}
+			}
+			case 'L' -> {
+				if (direction.equals(Direction.WEST) || direction.equals(Direction.SOUTH)) {
+					d = reflect(direction.unitVector, 1);
+				}
+			}
+			case 'J' -> {
+				if (direction.equals(Direction.EAST) || direction.equals(Direction.SOUTH)) {
+					d = reflect(direction.unitVector, -1);
+				}
+			}
+			case '7' -> {
+				if (direction.equals(Direction.EAST) || direction.equals(Direction.NORTH)) {
+					d = reflect(direction.unitVector, 1);
+				}
+			}
+		}
+		return d == null ? null : new Pair<Coordinate, Day10.Direction>(c.add(d.unitVector), d);
+	}
+
+	private boolean isValidCoordinate(Coordinate c) {
+		if (c.x >= grid.length || c.x < 0 || c.y >= grid[0].length || c.y < 0) {
+			return false;
+		}
+		if (grid[c.x][c.y] == '.') {
+			return false;
+		}
+		return true;
+	}
+
+	private Direction reflect(Coordinate unitVector, int magnitude) {
+		Coordinate newUnitVector = new Coordinate(unitVector.y * magnitude, unitVector.x * magnitude);
+		return Direction.getDirectionFromVector(newUnitVector);
+	}
+
+	public enum Direction {
+		NORTH(-1, 0),
+		EAST(0, 1),
+		SOUTH(1, 0),
+		WEST(0, -1);
+
+		public final Coordinate unitVector;
+		public final Coordinate zeroVector;
+
+		private Direction(int x, int y) {
+			this.unitVector = new Coordinate(x, y);
+			this.zeroVector = new Coordinate(0, 0);
+		}
+
+		public static Direction getDirectionFromVector(Coordinate vector) {
+			if (vector.x == 0) {
+				if (vector.y > 0) {
+					return EAST;
+				}
+				return WEST;
+			}
+			if (vector.y == 0) {
+				if (vector.x > 0) {
+					return SOUTH;
+				}
+				return NORTH;
+			}
+			return null;
+		}
+
 	}
 }
--- solutions/src/main/java/Solutions/Main.java
+++ solutions/src/main/java/Solutions/Main.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 class Main {
 	public static void main(String[] args) throws IOException {
-		List<String> inputs = new ReadFileHelper("inputs/day5.txt").getAllLines();
-		System.out.println(new BigDecimal(new Day5Part2(inputs).getResult()).toPlainString());
+		List<String> inputs = new ReadFileHelper("inputs/day10.txt").getAllLines();
+		System.out.println(new BigDecimal(new Day10(inputs).getResult()).toPlainString());
 	}
 }
