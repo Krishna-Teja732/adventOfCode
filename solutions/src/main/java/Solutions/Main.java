7a95cb69e4b0bbeca1d1c2d1efcc2c55f21d15fe
Krishna Teja <b.krishnateja732@gmail.com>
2024-12-01 14:13:34 +0530

    added day 1 sol

--- solutions/src/main/java/Solutions/Main.java
+++ solutions/src/main/java/Solutions/Main.java
@@ -1,15 +1,15 @@
 package Solutions;
 
 import Solutions.utils.ReadFileHelper;
-import Solutions.aoc23.*;
+import Solutions.aoc24.*;
 
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.List;
 
 class Main {
-	public static void main(String[] args) throws IOException {
-		List<String> inputs = new ReadFileHelper("inputs/day14example.txt").getAllLines();
-		System.out.println(new BigDecimal(new Day14(inputs).getResult()).toPlainString());
+	public static void main(String[] args) throws IOException, Exception {
+		List<String> inputs = new ReadFileHelper("inputs/2024/day1.txt").getAllLines();
+		System.out.println(new BigDecimal(new Day1(inputs).getResult()).toPlainString());
 	}
 }
--- solutions/src/main/java/Solutions/aoc24/Day1.java
+++ solutions/src/main/java/Solutions/aoc24/Day1.java
@@ -1,5 +1,69 @@
 package Solutions.aoc24;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.PriorityQueue;
+
 public class Day1 {
+
+	List<String> inputs;
+
+	public Day1(List<String> inputs) {
+		this.inputs = inputs;
+	}
+
+	// This function returns the wrong result
+	public double getResultV2() throws Exception {
+		long sum1 = 0;
+		long sum2 = 0;
+
+		for (String line : this.inputs) {
+			int splitIndex = line.indexOf(" ");
+			long val1 = (Long.parseUnsignedLong(line.substring(0, splitIndex).strip()));
+			long val2 = (Long.parseUnsignedLong(line.substring(splitIndex + 1).strip()));
+
+			sum1 = sum1 + val1;
+			sum2 = sum2 + val2;
+		}
+		return Math.abs(sum1 - sum2);
+	}
+
+	public double getResult() {
+		double result = 0;
+
+		PriorityQueue<Integer> arr1 = new PriorityQueue<>((num1, num2) -> num1 - num2);
+		PriorityQueue<Integer> arr2 = new PriorityQueue<>((num1, num2) -> num1 - num2);
+		for (String line : this.inputs) {
+			int splitIndex = line.indexOf(" ");
+			arr1.add(Integer.parseInt(line.substring(0, splitIndex).strip()));
+			arr2.add(Integer.parseInt(line.substring(splitIndex + 1).strip()));
+		}
+
+		while (!arr1.isEmpty()) {
+			result += Math.abs(arr1.remove() - arr2.remove());
+		}
+
+		return result;
+	}
+
+	public double getResultPart2() {
+		double result = 0;
+
+		HashMap<Integer, Integer> map1 = new HashMap<>();
+		HashMap<Integer, Integer> map2 = new HashMap<>();
+		for (String line : this.inputs) {
+			int splitIndex = line.indexOf(" ");
+			int key1 = Integer.parseInt(line.substring(0, splitIndex).strip());
+			int key2 = Integer.parseInt(line.substring(splitIndex + 1).strip());
+
+			map1.put(key1, map1.getOrDefault(key1, 0) + 1);
+			map2.put(key2, map2.getOrDefault(key2, 0) + 1);
+		}
+
+		for (int key : map1.keySet()) {
+			result += key * map2.getOrDefault(key, 0) * map1.get(key);
+		}
+		return result;
+	}
 
 }
