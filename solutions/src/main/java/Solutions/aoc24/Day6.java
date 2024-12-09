ccf0e900429041853538c7eb2ffb9f063add28d8
Krishna Teja <b.krishnateja732@gmail.com>
2024-12-09 12:29:10 +0530

    Added day 6 part 2

--- solutions/src/main/java/Solutions/aoc24/Day6.java
+++ solutions/src/main/java/Solutions/aoc24/Day6.java
@@ -2,6 +2,8 @@ package Solutions.aoc24;
 
 import Solutions.utils.Coordinate;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 public class Day6 {
@@ -38,14 +40,9 @@ public class Day6 {
 		return result;
 	}
 
-	public int getResultPart2() {
-		return -1;
-	}
-
 	// Mark all visited coordinates
 	// If path is a loop return true, flase otherwise
-	private boolean traverseGrid(Coordinate pos, Coordinate direction) {
-		Coordinate startCoordinate = new Coordinate(pos);
+	private void traverseGrid(Coordinate pos, Coordinate direction) {
 		while (isValidCoordinate(pos)) {
 			// Mark position as visited
 			this.grid[pos.x][pos.y] = 'x';
@@ -59,15 +56,55 @@ public class Day6 {
 				pos = pos.add(direction);
 			}
 
-			// Loop present
-			if (startCoordinate.equals(pos) && newDirection.equals(direction)) {
-				break;
-			}
+			direction = newDirection;
+		}
+	}
 
+	public int getResultPart2() {
+		int result = 0;
+		for (int x = 0; x < this.gridRowLen; x++) {
+			for (int y = 0; y < this.gridColLen; y++) {
+				if (this.grid[x][y] == '#'
+						|| (this.startPosition.x == x && this.startPosition.y == y)) {
+					continue;
+				}
+
+				this.grid[x][y] = '#';
+				if (hasLoop()) {
+					result++;
+				}
+				this.grid[x][y] = '.';
+			}
+		}
+		return result;
+	}
+
+	private boolean hasLoop() {
+		Coordinate pos = new Coordinate(this.startPosition);
+		Coordinate direction = new Coordinate(-1, 0);
+		HashMap<Coordinate, HashSet<Coordinate>> posDirMap = new HashMap<>();
+
+		while (isValidCoordinate(pos)) {
+			if (posDirMap.containsKey(pos) && posDirMap.get(pos).contains(direction)) {
+				return true;
+			}
+			if (!posDirMap.containsKey(pos)) {
+				posDirMap.put(pos, new HashSet<>());
+			}
+			posDirMap.get(pos).add(direction);
+
+			// Look ahead one position to get the direction to move
+			Coordinate newDirection = udpateDirection(pos.add(direction), direction);
+
+			// Move only if the direction remains unchanged
+			// This condition prevents us from collinding with #
+			if (newDirection == direction) {
+				pos = pos.add(direction);
+			}
 			direction = newDirection;
 		}
 
-		return startCoordinate.equals(pos);
+		return false;
 	}
 
 	private Coordinate udpateDirection(Coordinate coord, Coordinate curDirection) {
@@ -77,10 +114,7 @@ public class Day6 {
 		}
 
 		// path is blocked, turn 90 deg right
-		int temp = -1 * curDirection.x;
-		curDirection.x = curDirection.y;
-		curDirection.y = temp;
-		return curDirection;
+		return new Coordinate(curDirection.y, curDirection.x * -1);
 	}
 
 	private boolean isValidCoordinate(Coordinate coord) {
